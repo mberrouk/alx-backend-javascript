8a7ad91d308e5e6768b18efbcc06e4b56600a925
mberrouk <mberrouk@student.1337.ma>
2024-10-04 20:59:46 +0000

    init

---
+++ 0x00-ES6_basic/10-loops.js
@@ -0,0 +1,9 @@
+export default function appendToEachArrayValue(array, appendString) {
+  const newArr = [];
+
+  for (const value of array) {
+    newArr.push(appendString + value);
+  }
+
+  return newArr;
+}
