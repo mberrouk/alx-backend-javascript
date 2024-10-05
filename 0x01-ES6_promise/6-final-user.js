b02fbf0c15317b6cc4345889908b45c79cd40270
mberrouk <mberrouk@student.1337.ma>
2024-10-05 20:26:19 +0100

    inint

---
+++ 0x01-ES6_promise/6-final-user.js
@@ -0,0 +1,17 @@
+import signUpUser from './4-user-promise';
+import uploadPhoto from './5-photo-reject';
+
+export default function handleProfileSignup(firstName, lastName, fileName) {
+  return Promise
+    .allSettled([signUpUser(firstName, lastName), uploadPhoto(fileName)])
+    .then((res) => {
+      const arr = [];
+      for (const prom of res) {
+        arr.push({
+          status: prom.status,
+          value: prom.status === 'fulfilled' ? prom.value : prom.reason,
+        });
+      }
+      return arr;
+    });
+}
