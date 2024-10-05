25cc88ae3b9f639cabeb7eaf2a76bff067ec5602
mberrouk <mberrouk@student.1337.ma>
2024-10-05 19:39:03 +0100

    init

--- 0x01-ES6_promise/3-all.js
+++ 0x01-ES6_promise/3-all.js
@@ -1,10 +1,7 @@
 import { uploadPhoto, createUser } from './utils';
 
 export default function handleProfileSignup() {
-  const upPhoto = uploadPhoto();
-  const creUser = createUser();
-
-  return Promise.all([upPhoto, creUser]).then((resArr) => {
+  return Promise.all([uploadPhoto(), createUser()]).then((resArr) => {
     console.log(resArr[0].body, resArr[1].firstName, resArr[1].lastName);
   }).catch(() => console.log('Signup system offline'));
 }
