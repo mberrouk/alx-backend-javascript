07f6587835bfaf356fd7b78519f93750d79bcdc9
mberrouk <mberrouk@student.1337.ma>
2024-10-14 18:40:14 +0100

    init

---
+++ 0x03-ES6_data_manipulation/9-groceries_list.js
@@ -0,0 +1,10 @@
+export default function groceriesList() {
+  const resMap = new Map();
+  resMap.set('Apples', 10);
+  resMap.set('Tomatoes', 10);
+  resMap.set('Pasta', 1);
+  resMap.set('Rice', 1);
+  resMap.set('Banana', 5);
+
+  return resMap;
+}
