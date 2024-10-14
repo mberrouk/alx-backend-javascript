e2a54e280ffc39a88aa9134b62a5a774d7914462
mberrouk <mberrouk@student.1337.ma>
2024-10-14 23:01:41 +0100

    init

---
+++ 0x02-ES6_classes/8-hbtn_class.js
@@ -0,0 +1,24 @@
+export default class HolbertonClass {
+  constructor(size, location) {
+    this.size = size;
+    this.location = location;
+  }
+
+  get size() {
+    return this._size;
+  }
+
+  set size(size) {
+    this._size = size;
+  }
+
+  [Symbol.toPrimitive](hint) {
+    if (hint === 'number') {
+      return this.size;
+    }
+    if (hint === 'string') {
+      return this.location;
+    }
+    return this;
+  }
+}
