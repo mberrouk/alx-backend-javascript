7f1a43cbe08f33b40bde12105ac06a55ffbc2795
mberrouk <mberrouk@student.1337.ma>
2024-10-14 19:42:36 +0100

    init

---
+++ 0x02-ES6_classes/4-pricing.js
@@ -0,0 +1,26 @@
+export default class Pricing {
+  constructor(amount, currency) {
+    this.amount = amount;
+    this.currency = currency;
+  }
+
+  get amount() {
+    return this._amount;
+  }
+
+  set amount(amount) {
+    this._amount = amount;
+  }
+
+  get currency() {
+    return this._currency;
+  }
+
+  set currency(currency) {
+    this._currency = currency;
+  }
+
+  displayFullPrice() {
+    return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+}
