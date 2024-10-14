9457c2756a7313dee6437fe870be472590de59e5
mberrouk <mberrouk@student.1337.ma>
2024-10-14 20:10:50 +0100

    init

--- 0x02-ES6_classes/4-pricing.js
+++ 0x02-ES6_classes/4-pricing.js
@@ -1,3 +1,5 @@
+import Currency from './3-currency.js'
+
 export default class Pricing {
   constructor(amount, currency) {
     this.amount = amount;
@@ -9,6 +11,9 @@ export default class Pricing {
   }
 
   set amount(amount) {
+    if (!(typeof amount === 'number')) {
+      throw new TypeError('amount must be a number');
+    }
     this._amount = amount;
   }
 
@@ -17,10 +22,24 @@ export default class Pricing {
   }
 
   set currency(currency) {
+    if (!(currency instanceof Currency)) {
+      throw new TypeError('currency must be a Currency');
+    }
     this._currency = currency;
   }
 
   displayFullPrice() {
     return `${this.amount} ${this.currency.displayFullCurrency()}`;
+  }
+
+  static convertPrice(amount, conversionRate) {
+    if (!(typeof amount === 'number')) {
+      throw new TypeError('amount must be a number');
+    }
+    if (!(typeof conversionRate === 'number')) {
+      throw new TypeError('conversionRate must be a number');
+    }
+
+    return amount * conversionRate;
   }
 }
